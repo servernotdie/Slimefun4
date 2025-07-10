0dee9e2dd2c56ee3b9584bb9c790973827041827
neokoni <dulin081682@126.com>
2025-07-10 18:07:33 +0800

    Fix: soulbound will not give back item and it's disappeared
    folia have not PlayerRespawnEvent
    
    at least now

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/SoulboundListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/SoulboundListener.java
@@ -56,6 +56,12 @@ public class SoulboundListener implements Listener {
 
         // Remove soulbound items from our drops
         e.getDrops().removeIf(itemStack -> SlimefunUtils.isSoulbound(itemStack, p.getWorld()));
+
+        if (Slimefun.getFoliaLib().isFolia()){  // only Folia can do
+            Slimefun.getFoliaLib().getScheduler().runLaterAsync(wrappedTask -> {
+                returnSoulboundItems(p);
+            }, 10);
+        }
     }
 
     @EventHandler
